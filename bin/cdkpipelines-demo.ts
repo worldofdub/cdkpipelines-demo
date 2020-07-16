f467a176cc612e6e52e1140d1fcf601eada8bcf2
Paul Hawkins <paul@worldofdub.org.uk>
2020-07-16 11:12:20 +1000

    Initial deployment of empty pipeline

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack';
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
-const app = new cdk.App();
-new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack');
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '924126403201', region: 'ap-southeast-2' },
+});
+
+app.synth();
+
